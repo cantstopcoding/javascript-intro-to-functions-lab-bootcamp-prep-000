3e57ea38a16e3171dc6d3ae920e523eb2de53ccc
Jonathan Koss <jonathan_koss@yahoo.com>
2018-09-29 02:14:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,13 @@ function shout(string) {
   return string.toUpperCase();
 }
 
+function whisper(string) {
+  return string.toLowerCase;
+}
 
+
+
+/*describe('whisper(string)', function() {
+  it('receives one argument and returns it in all lowercase', function() {
+    expect(whisper('HELLO')).toEqual('hello')
+  })*/
